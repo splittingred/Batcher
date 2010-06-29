73faad3bdd2db8360acbec0f034b1f7be4423eb0
Shaun McCormick <shaun@modxcms.com>
2010-06-29 09:29:12 -0500

    Added French translation

---
+++ core/components/batcher/lexicon/fr/default.inc.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Batcher
+ *
+ * Copyright 2010 by Shaun McCormick <shaun@modxcms.com>
+ *
+ * This file is part of Batcher, a batch resource editing Extra.
+ *
+ * Batcher is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Batcher is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Batcher; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package batcher
+ */
+/**
+ * Default French language translation
+ *
+ * @package batcher
+ * @subpackage lexicon
+ * @language fr
+ */
+$_lang['batcher'] = 'Batcher';
+$_lang['batcher.bulk_actions'] = 'Actions de masse';
+$_lang['batcher.cacheable'] = 'Placer dans le cache';
+$_lang['batcher.change_authors'] = 'Changer les auteurs';
+$_lang['batcher.change_dates'] = 'Changer les dates';
+$_lang['batcher.change_parent'] = 'Changer le document parent';
+$_lang['batcher.change_template'] = 'Changer le modèle';
+$_lang['batcher.createdby'] = 'Créé par';
+$_lang['batcher.createdon'] = 'Créé le';
+$_lang['batcher.deleted'] = 'Supprimé';
+$_lang['batcher.editedby'] = 'Édité par';
+$_lang['batcher.editedon'] = 'Édité le';
+$_lang['batcher.filter_by_template'] = 'Trier par modèle';
+$_lang['batcher.hidemenu'] = 'Masquer dans le menu';
+$_lang['batcher.intro_msg'] = 'Lancer l\'action de masse sur ces ressources.';
+$_lang['batcher.menu_desc'] = 'Édition de ressources en masse.';
+$_lang['batcher.parent'] = 'Document parent';
+$_lang['batcher.parent_err_nf'] = 'Parent non trouvé.';
+$_lang['batcher.parent_err_ns'] = 'Parent non spécifié.';
+$_lang['batcher.pub_date'] = 'Date de publication';
+$_lang['batcher.published'] = 'Publié';
+$_lang['batcher.publishedby'] = 'Publié par';
+$_lang['batcher.richtext'] = 'Éditeur richtext activé';
+$_lang['batcher.searchable'] = 'Cherchable';
+$_lang['batcher.template'] = 'Modèle';
+$_lang['batcher.template_err_nf'] = 'Modèle non trouvé.';
+$_lang['batcher.template_err_ns'] = 'Modèle non spécifié.';
+$_lang['batcher.toggle'] = 'Intervertir';
+$_lang['batcher.uncacheable'] = 'Ne pas placer dans le cache';
+$_lang['batcher.undeleted'] = 'Non supprimé';
+$_lang['batcher.unhidemenu'] = 'Affiché dans le menu';
+$_lang['batcher.unpub_date'] = 'Date de dépublication';
+$_lang['batcher.unpublished'] = 'Dépublié';
+$_lang['batcher.unrichtext'] = 'Éditeur richtext désactivé';
+$_lang['batcher.unsearchable'] = 'Non cherchable';
+$_lang['batcher.user_err_nf'] = 'Utilisateur non trouvé.';
