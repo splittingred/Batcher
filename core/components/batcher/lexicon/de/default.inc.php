86f1a042850a388ff4ae6616589402033b2fea63
Jan-Christoph Ihrens <ihrens@cc-services.de>
2010-11-10 14:25:27 +0100

    Updated German translation for Batcher
    Signed-off-by: Jan-Christoph Ihrens <ihrens@cc-services.de>

--- core/components/batcher/lexicon/de/default.inc.php
+++ core/components/batcher/lexicon/de/default.inc.php
@@ -22,17 +22,25 @@
  * @package batcher
  */
 /**
- * German language translation
+ * Default German language translation
  * 
  * @package batcher
  * @subpackage lexicon
  * @language de
  */
 $_lang['batcher'] = 'Batcher';
+$_lang['batcher.action_err_ns'] = 'Bitte wählen Sie eine Aktion.';
+$_lang['batcher.and_others'] = 'und [[+count]] weitere...';
 $_lang['batcher.bulk_actions'] = 'Stapel-Aktionen';
 $_lang['batcher.cacheable'] = 'Cache aktiv';
+$_lang['batcher.category'] = 'Kategorie';
+$_lang['batcher.category_err_ns'] = 'Kategorie nicht angegeben.';
+$_lang['batcher.category_err_nf'] = 'Kategorie mit der ID [[+id]] nicht gefunden';
 $_lang['batcher.change_authors'] = 'Autoren ändern';
+$_lang['batcher.change_category'] = 'Kategorie ändern';
 $_lang['batcher.change_dates'] = 'Daten ändern';
+$_lang['batcher.change_default_tv_values'] = 'TV-Standardwerte ändern';
+$_lang['batcher.change_tv_values'] = 'TV-Werte ändern';
 $_lang['batcher.change_parent'] = 'Eltern-Ressource ändern';
 $_lang['batcher.change_template'] = 'Template ändern';
 $_lang['batcher.createdby'] = 'Erstellt von';
@@ -50,11 +58,21 @@ $_lang['batcher.parent_err_ns'] = 'Eltern-Ressource nicht angegeben.';
 $_lang['batcher.pub_date'] = 'Veröffentlichungsdatum';
 $_lang['batcher.published'] = 'Veröffentlicht';
 $_lang['batcher.publishedby'] = 'Veröffentlicht von';
+$_lang['batcher.resources'] = 'Ressourcen';
+$_lang['batcher.resources_affect'] = 'Dies wirkt sich auf die folgenden Ressourcen aus:';
+$_lang['batcher.resources_err_ns'] = 'Bitte wählen Sie Ressourcen aus, für die diese Aktion ausgeführt werden soll.';
 $_lang['batcher.richtext'] = 'Rich-Text-Editor aktiviert';
 $_lang['batcher.searchable'] = 'Durchsuchbar';
 $_lang['batcher.template'] = 'Template';
 $_lang['batcher.template_err_nf'] = 'Template nicht gefunden.';
 $_lang['batcher.template_err_ns'] = 'Template nicht angegeben.';
+$_lang['batcher.template.tvdefaults.intro_msg'] = 'Hier können Sie die Standardwerte der Template-Variablen für dieses Template festlegen. Wählen Sie die Template-Variablen aus, die Sie ändern möchten.';
+$_lang['batcher.template.tvs.intro_msg'] = 'Hier können Sie die Werte der Template-Variablen für alle Ressourcen, die dieses Template verwenden, festlegen. Wählen Sie die Template-Variablen aus, die Sie ändern möchten.';
+$_lang['batcher.templates'] = 'Templates';
+$_lang['batcher.templates_err_ns'] = 'Bitte wählen Sie Templates aus, für die diese Aktion ausgeführt werden soll.';
+$_lang['batcher.templates.intro_msg'] = 'Hier können Sie einige Einstellungen für mehrere Templates gleichzeitig bearbeiten.';
+$_lang['batcher.tvs'] = 'Template-Variablen';
+$_lang['batcher.tvs_err_ns'] = 'Keine Template-Variable angegeben!';
 $_lang['batcher.toggle'] = 'Umschalten';
 $_lang['batcher.uncacheable'] = 'Cache nicht aktiv';
 $_lang['batcher.undeleted'] = 'Nicht gelöscht';
